a541655962576d0835beb044258825ed6558ef66
pleaseDoNotTheCat <85490435+pleaseDoNotTheCat@users.noreply.github.com>
2021-09-09 15:01:46 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const aoijs = require("aoi.js")
+
+const bot = new aoijs.Bot({
+    token: "ODg1MTc5NDg1MjQ1MjgwMzA2.YTjRow.cRbl2kGR9HqYgg3w0tleETnNrHQ", // Token
+    prefix: "$getServerVar[prefix]", // Prefix
+    mobile: true // Mobile presence
+})
+
+bot.onMessage() // Initial Command line
+
+bot.loadCommands('./commands')
+
+bot.readyCommand({
+    channel: "", // Message Channel
+    code: `$log[Uruchomiono! Zalogowano jako $userTag[$clientID]]
+    $channelSendMessage[882659343391158282; jestem debilem i informuje ze jestem wlaczony.!;no]` // Message
+})
+
+bot.variables({
+   prefix: "*",
+   rank: "User",
+   ideach: "0" 
+})
+
+bot.botJoinCommand({
+    channel: "",
+    code: `$channelSendMessage[882942379684933669;
+    Nazwa: $serverName[$guildID]
+    ID: $guildID
+    ID Właściciela: $ownerName]`
+    })
+
+    bot.botLeaveCommand({
+        channel: "882942379684933670",
+        code: `$channelSendMessage[882942379684933669;
+            Nazwa: $serverName[$guildID]
+            ID: $guildID
+            ID Właściciela: $ownerName;no]`
+        })
